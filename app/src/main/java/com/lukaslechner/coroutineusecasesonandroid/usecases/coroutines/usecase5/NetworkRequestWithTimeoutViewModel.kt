7d8eeafbf3d6a8a194df7492c07d2b829d303c65
LBRBS <lysanderarbis@gmail.com>
2023-04-20 21:30:24 +0300

    06 - Higher-Order Functions 07 - Using Room with Coroutines

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,63 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import timber.log.Timber
+import java.sql.Time
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
 
+        //usingWithTimeout(timeout)
+        usingWithTimeoutOrNull(timeout)
+    }
+
+    private fun usingWithTimeout(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersion = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                if (recentAndroidVersion != null) {
+                    uiState.value = UiState.Success(recentAndroidVersion)
+                } else {
+                    uiState.value = UiState.Error("Network Request Failed!")
+                }
+            } catch (timeoutCancellationException: TimeoutCancellationException) {
+                Timber.e(timeoutCancellationException)
+                uiState.value = UiState.Error("Network Request Timeout!")
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network Request Failed!")
+            }
+        }
+    }
+
+    private fun usingWithTimeoutOrNull(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersion = withTimeoutOrNull(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                if (recentAndroidVersion != null) {
+                    uiState.value = UiState.Success(recentAndroidVersion)
+                } else {
+                    uiState.value = UiState.Error("Network Request Failed!")
+                }
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network Request Failed!")
+            }
+        }
     }
 
 }

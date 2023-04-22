bc16b5a9c64aa070da1eceaf8d621e05380d4ba5
LBRBS <lysanderarbis@gmail.com>
2023-04-22 21:15:21 +0300

    09 - Structured Concurrency and Coroutine Scopes 10 - Coroutines Cancellation

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase11/CooperativeCancellationViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase11/CooperativeCancellationViewModel.kt
@@ -3,20 +3,64 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import timber.log.Timber
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CooperativeCancellationViewModel(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : ViewModel() {
 
-    fun performCalculation(factorialOf: Int) {
+    private var calculationJob: Job? = null
 
+    fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
+
+        calculationJob = viewModelScope.launch {
+            try {
+                var result: BigInteger = BigInteger.ZERO
+                val computationDuration = measureTimeMillis {
+                    result = calculateFactorialOf(factorialOf)
+                }
+                var resultString = ""
+                var stringConversionDuration = measureTimeMillis {
+                    resultString = convertToString(result)
+                }
+                uiState.value =
+                    UiState.Success(resultString, computationDuration, stringConversionDuration)
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Calculation Error")
+            }
+        }
+
+        calculationJob?.invokeOnCompletion { throwable ->
+            if (throwable is CancellationException) {
+                Timber.d("Calculation was Cancelled!")
+            }
+        }
     }
 
     fun cancelCalculation() {
-
+        calculationJob?.cancel()
     }
+
+    private suspend fun calculateFactorialOf(number: Int) =
+        withContext(Dispatchers.Default) {
+            var factorial = BigInteger.ONE
+            for (i in 1..number) {
+                factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+            }
+            factorial
+        }
+
+    private suspend fun convertToString(number: BigInteger) =
+        withContext(Dispatchers.Default) {
+            number.toString()
+        }
+
 
     fun uiState(): LiveData<UiState> = uiState
 
